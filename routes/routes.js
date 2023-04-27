fe16edd0a42f32565dbdc1a19250681174e3b001
Bhavesh Dewangan <ybkdddd@gmail.com>
2023-04-27 23:58:53 +0530

    route fix

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var router = express.Router();
+const User_Controller = require("../controllers/UserController");
+
+const Post_controller = require("../controllers/PostController");
+const passport = require("passport");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -9,12 +13,10 @@ router.get("/", function (req, res, next) {
 router.post("/signup", User_Controller.sign_up_post);
 router.post("/login", User_Controller.log_in_post);
 
-router.post("/signup", User_Controller.sign_up_post);
-router.post("/login", User_Controller.log_in_post);
-
 router.post(
   "/posts",
   passport.authenticate("jwt", { session: false }),
   Post_controller.create_post
 );
+
 module.exports = router;
